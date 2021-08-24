aba383ae0303f2afe0c3862f28ec4ad6258b84d6
MirandaRamosC <A01641733@itesm.mx>
2021-08-24 04:51:45 +0000

    tarea

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+    persona = int(input("Dame tu edad:"))
+    año = int(input("Dame el año actual:"))
+    resultado = (100-persona)+año
+    print("Cumplirás 100 años en el año:" + str(resultado) )
 
 
 
