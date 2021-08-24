73fe2693ad439e067e07e7bb9d93d4f880a4c042
MirandaRamosC <A01641733@itesm.mx>
2021-08-24 04:57:12 +0000

    tarea

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    peso = int(input("Dame tu el peso inicial:"))
+    final = int(input("Dame el peso final:"))
+    meses = int(input("Dame la cantidad de meses:"))
+    resultado = (peso-final)/meses
+    print("Lo que debes de bajar por mes es:" + str(resultado) )
 
 
 
